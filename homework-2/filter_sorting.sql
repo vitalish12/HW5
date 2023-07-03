2fa529bdd589bf75bc0d5fd49feb3aafbdaa888d
vitali <sh015@mail.ru>
2023-07-03 22:45:51 +0300

    1

---
+++ homework-2/filter_sorting.sql
@@ -0,0 +1,32 @@
+-- Напишите запросы, которые выводят следующую информацию:
+-- 1. заказы, доставленные в страны France, Germany, Spain (таблица orders, колонка ship_country)
+select *
+FROM orders
+where ship_country in ('France', 'Germany', 'Spain')
+
+-- 2. уникальные города и страны, куда отправлялись заказы, отсортировать по странам и городам
+-- (таблица orders, колонки ship_country, ship_city)
+select distinct ship_city, ship_country
+from orders
+order by ship_city, ship_country
+
+
+-- 3. сколько дней в среднем уходит на доставку товара в Германию
+-- (таблица orders, колонки order_date, shipped_date, ship_country)
+select floor(avg(shipped_date - order_date))
+from orders
+where ship_country in ('Germany')
+
+
+-- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи
+-- (таблица products, колонки unit_price, discontinued не равно 1)
+select min(unit_price), max(unit_price)
+from products
+where discontinued <> 1
+
+
+-- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20
+-- (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+select min(unit_price), max(unit_price)
+from products
+where discontinued <> 1 and units_in_stock > 20
